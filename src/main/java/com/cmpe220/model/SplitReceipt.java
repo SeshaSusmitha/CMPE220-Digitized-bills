ad6e671b8b85f283e3f5d389dfba7d9361347066
Poojabafnaprakash <poojabafnaprakash@gmail.com>
2017-04-02 16:28:38 -0700

    added getter and setter for splitreceipt table

--- src/main/java/com/cmpe220/model/SplitReceipt.java
+++ src/main/java/com/cmpe220/model/SplitReceipt.java
@@ -36,5 +36,70 @@ public class SplitReceipt {
 	
 	@Column(name="amount")
 	float amount;
+	
+	public SplitReceipt() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public SplitReceipt(int receiptId, User userId, Bill billId, Friend friendId, Items itemId, float amount) {
+		super();
+		this.receiptId = receiptId;
+		this.userId = userId;
+		this.billId = billId;
+		this.friendId = friendId;
+		this.itemId = itemId;
+		this.amount = amount;
+	}
+
+	public int getReceiptId() {
+		return receiptId;
+	}
+
+	public void setReceiptId(int receiptId) {
+		this.receiptId = receiptId;
+	}
+
+	public User getUserId() {
+		return userId;
+	}
+
+	public void setUserId(User userId) {
+		this.userId = userId;
+	}
+
+	public Bill getBillId() {
+		return billId;
+	}
+
+	public void setBillId(Bill billId) {
+		this.billId = billId;
+	}
+
+	public Friend getFriendId() {
+		return friendId;
+	}
+
+	public void setFriendId(Friend friendId) {
+		this.friendId = friendId;
+	}
+
+	public Items getItemId() {
+		return itemId;
+	}
+
+	public void setItemId(Items itemId) {
+		this.itemId = itemId;
+	}
+
+	public float getAmount() {
+		return amount;
+	}
+
+	public void setAmount(float amount) {
+		this.amount = amount;
+	}
+
+	
 
 }
